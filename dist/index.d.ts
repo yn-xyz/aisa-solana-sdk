84454a0877115eb6b3ffbec366eee24a7e0d15ff
wxd56987 <672514143@qq.com>
2025-03-13 14:16:49 +0800

    Remove dist directory from git tracking and add it to .gitignore

--- dist/index.d.ts
+++
@@ -1,3 +0,0 @@
-export { MainAccountTxHandler } from "./txhandler/mainAccHandler";
-export { SubAccountTxHandler } from "./txhandler/subAccHandler";
-//# sourceMappingURL=index.d.ts.map
--- dist/utils/common.d.ts
+++
@@ -1,6 +0,0 @@
-import { PublicKey } from "@solana/web3.js";
-import { Connection } from "@solana/web3.js";
-export declare function deriveMainAccount(uuid: Uint8Array): PublicKey;
-export declare function deriveSubAccount(mainAccount: PublicKey, agent: PublicKey): PublicKey;
-export declare function getTokenBalance(connection: Connection, tokenAccount: PublicKey): Promise<number>;
-//# sourceMappingURL=common.d.ts.map
